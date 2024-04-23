e9abdb8eeb3e22d3dc8c7df81f6fb9b212471838
anthonyccosta <ccosta.anthony@gmail.com>
2024-04-22 22:16:42 -0300

    primeiro commit

---
+++ fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+        Console.Write("Digite a posição desejada: ");
+        int posicao = int.Parse(Console.ReadLine());
+
+        if (posicao < 1)
+        {
+            Console.WriteLine("Erro: Posição deve ser maior que zero.");
+        }
+
+        Console.Write("sequência fibonacci até a posição " + posicao + ": ");
+        int primeiro = 0;
+        int ultimo = 1;
+
+        for (int i = 0; i < posicao; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    Console.Write(primeiro + " ");
+                    break;
+                case 1:
+                    Console.Write(ultimo + " ");
+                    break;
+                default:
+                    int proximoValor = primeiro + ultimo;
+                    Console.Write(proximoValor + " ");
+                    primeiro = ultimo;
+                    ultimo = proximoValor;
+                    break;
+            }
+        }
