9bd500ce5d95cbba3df331cb1947f4095aea972c
anthonyccosta <ccosta.anthony@gmail.com>
2024-04-23 09:38:44 -0300

    segundo commit

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -1,32 +1,43 @@
-﻿
+﻿int penultimo = -1, ultimo = 1, atual;
 
         Console.Write("Digite a posição desejada: ");
         int posicao = int.Parse(Console.ReadLine());
 
-        if (posicao < 1)
-        {
-            Console.WriteLine("Erro: Posição deve ser maior que zero.");
-        }
+        //if (posicao < 1)
+        //{
+        //    Console.WriteLine("Erro: Posição deve ser maior que zero.");
+        //}
 
-        Console.Write("sequência fibonacci até a posição " + posicao + ": ");
-        int primeiro = 0;
-        int ultimo = 1;
+        //Console.Write("sequência fibonacci até a posição " + posicao + ": ");
+        //int primeiro = 0;
+        //int ultimo = 1;
+
 
         for (int i = 0; i < posicao; i++)
         {
-            switch (i)
-            {
-                case 0:
-                    Console.Write(primeiro + " ");
-                    break;
-                case 1:
-                    Console.Write(ultimo + " ");
-                    break;
-                default:
-                    int proximoValor = primeiro + ultimo;
-                    Console.Write(proximoValor + " ");
-                    primeiro = ultimo;
-                    ultimo = proximoValor;
-                    break;
-            }
+            atual = penultimo + ultimo;
+            Console.WriteLine(atual);
+            penultimo = ultimo;
+            ultimo = atual;
         }
+
+//for (int i = 0; i < posicao; i++)
+{
+    //switch (i)
+    //{
+    //    case 0:
+    //        Console.Write(primeiro);
+    //        break;
+    //    case 1:
+    //        Console.Write(ultimo);
+    //        break;
+    //    default:
+    //        int proximoValor = primeiro + ultimo;
+    //        Console.Write(proximoValor);
+    //        primeiro = ultimo;
+    //        ultimo = proximoValor;
+    //        break;
+    //}
+
+    
+}
--- fibonacci/obj/Debug/net6.0/fibonacci.AssemblyInfo.cs
+++ fibonacci/obj/Debug/net6.0/fibonacci.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fibonacci")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9abdb8eeb3e22d3dc8c7df81f6fb9b212471838")]
 [assembly: System.Reflection.AssemblyProductAttribute("fibonacci")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fibonacci")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
